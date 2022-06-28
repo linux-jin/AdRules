48ca90f9ad17e347b98efc2bd69d3d09600558f4
hacamer <hcamer145@gmail.com>
2022-06-29 01:26:19 +0800

    Fix bug

--- script/rebuilt-dns-list.sh
+++ script/rebuilt-dns-list.sh
@@ -15,9 +15,9 @@ mv -f 1.txt pre-rules.txt
 
 for i in ${!wl0[@]} ${!wl1[@]} ${!wl2[@]}
 do
-  sed -i '/$wl0/d' pre-rules.txt &
-  sed -i '/$wl2/d' pre-rules.txt &
-  sed -i '/.*$wl1/d' pre-rules.txt &
+  sed -i "/$wl0/d" pre-rules.txt &
+  sed -i "/$wl2/d" pre-rules.txt &
+  sed -i "/.*$wl1/d" pre-rules.txt &
 done
 wait
 i=`cat pre-rules.txt|grep -v "#"|grep -v "\/"|grep -v "^\."|sed 's/.*#.*//g' | sed '/^$/d'`
